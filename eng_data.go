588a3fa38227e851742fc94c3f82dd5226bb07e3
liuzhanliang <liuzhanliang@baidu.com>
2018-02-26 15:57:02 +0800

    https://en.wikipedia.org/wiki/Wikipedia:List_of_English_contractions

--- eng_data.go
+++ eng_data.go
@@ -1,5 +1,6 @@
 package tokenizer
 
+// https://en.wikipedia.org/wiki/Wikipedia:List_of_English_contractions
 var EngContractions string = `ain't	ai n't	am not
 aren't	are n't	are not
 can't	ca n't	can not
