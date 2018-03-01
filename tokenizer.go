844d503f074117a6e2e8f32120dd5f4c84acc89f
liuzhanliang <liuzhanliang@baidu.com>
2018-03-01 10:53:43 +0800

    more test cases, number followed by unit

--- tokenizer.go
+++ tokenizer.go
@@ -29,7 +29,9 @@ func TokenizePro(text string) []*Token {
 	for seg.Segment() {
 		text := seg.Text()
 		lowered := strings.ToLower(text)
-		if items, has := Contractions["eng"][lowered]; has {
+		items, has := Contractions["eng"][lowered]
+		switch {
+		case has: // deal with contractions
 			pos := 0
 			for i, term := range items.Terms {
 				txt := text[pos : pos+len(term)]
@@ -40,14 +42,12 @@ func TokenizePro(text string) []*Token {
 				ret = append(ret, &Token{Text: txt, Norm: norm})
 				pos += len(term)
 			}
-		} else {
 			// https://en.wikipedia.org/wiki/English_possessive
-			if strings.HasSuffix(lowered, `'s`) {
-				ret = append(ret, &Token{Text: text[:len(text)-2]})
-				ret = append(ret, &Token{Text: text[len(text)-2:]})
-			} else {
-				ret = append(ret, &Token{Text: text})
-			}
+		case strings.HasSuffix(lowered, `'s`):
+			ret = append(ret, &Token{Text: text[:len(text)-2]})
+			ret = append(ret, &Token{Text: text[len(text)-2:]})
+		default:
+			ret = append(ret, &Token{Text: text})
 		}
 	}
 	return ret
--- tokenizer_test.go
+++ tokenizer_test.go
@@ -8,9 +8,12 @@ func TestTokenize(t *testing.T) {
 	cases := []string{
 		`I'm ok. but I still don't know who's my boy.`,
 		`This is Zhanliang's book. the boys' books.`,
+		`自建房2樓3室2廳1衛1廚92.00㎡戶型圖，2,123km到北京`,
+		`自建房2樓3室2廳1衛1廚92.00㎡戶型圖，92km到北京`,
+		`自建房2樓3室2廳1衛1廚92.00㎡戶型圖，92.54km到北京`,
 	}
 	for _, c := range cases {
-		ret := TokenizePro(c)
+		ret := Tokenize(c)
 		t.Log(ret)
 	}
 }
